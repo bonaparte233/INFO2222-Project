1542e8106860ae1828eef802aaa85bd7d121408f
Wentao0 <102937235+Wentao0@users.noreply.github.com>
2022-04-10 16:47:21 +0800

    Add files via upload

--- db_initialize.py
+++ db_initialize.py
@@ -1,9 +1,9 @@
 import sqlite3
 con = sqlite3.connect('database.db')
 con.execute("DROP TABLE IF EXISTS Users;")
-con.execute("CREATE TABLE Users (username VARCHAR(200) PRIMARY KEY, password VARCHAR(200), admin CHAR(1))")
-con.execute("INSERT INTO Users (username, password, admin) VALUES ('WentaoGao', '123456', '1')")
-con.execute("INSERT INTO Users (username, password, admin) VALUES ('James', '123456', '1')")
+con.execute("CREATE TABLE Users (username VARCHAR(200) PRIMARY KEY, password VARCHAR(200), admin CHAR(1), salt VARCHAR(200))")
+con.execute("INSERT INTO Users (username, password, admin, salt) VALUES ('WentaoGao', '123456', '1', 'xyz')")
+con.execute("INSERT INTO Users (username, password, admin, salt) VALUES ('James', '123456', '1', 'xyz')")
 con.commit()
 
 con.execute("DROP TABLE IF EXISTS Friends;")
